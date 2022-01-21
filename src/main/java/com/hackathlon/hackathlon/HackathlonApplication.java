ec6d37300d58d61fee76260a231843a973834515
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-01-21 10:55:30 +0100

    Initial Commit

---
+++ src/main/java/com/hackathlon/hackathlon/HackathlonApplication.java
@@ -0,0 +1,13 @@
+package com.hackathlon.hackathlon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathlonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathlonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathlon/hackathlon/HackathlonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathlon.hackathlon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathlonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
