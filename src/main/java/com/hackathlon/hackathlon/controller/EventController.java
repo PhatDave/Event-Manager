e393e7dbfeb2622a5fb616619b79c871286309cc
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-01-26 10:36:02 +0100

    Complete SBSS-01

--- src/main/java/com/hackathlon/hackathlon/controller/EventController.java
+++ src/main/java/com/hackathlon/hackathlon/controller/EventController.java
@@ -1,13 +1,14 @@
 package com.hackathlon.hackathlon.controller;
 
 import com.hackathlon.hackathlon.dto.requests.*;
+import com.hackathlon.hackathlon.entity.*;
 import com.hackathlon.hackathlon.service.*;
 import lombok.*;
 import org.springframework.dao.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.*;
+import java.net.*;
 
 @RestController
 @RequestMapping("/event")
@@ -19,8 +20,8 @@ public class EventController {
     private ResponseEntity<?> create(@RequestBody EventRequestDto eventRequestDto) {
         try {
             System.out.println(eventRequestDto);
-            eventService.create(eventRequestDto);
-            return new ResponseEntity<>(HttpStatus.CREATED);
+            Event event = eventService.create(eventRequestDto);
+            return ResponseEntity.created(URI.create("/event/" + event.getID())).body("");
         }
         catch (DataIntegrityViolationException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
