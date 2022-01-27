bcd0f29307aceb80fb768aa46481a6e926d12f00
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-01-27 12:22:19 +0100

    Add error checking and multiple return codes

--- src/main/java/com/hackathlon/hackathlon/controller/RegistrationController.java
+++ src/main/java/com/hackathlon/hackathlon/controller/RegistrationController.java
@@ -1,28 +1,38 @@
 package com.hackathlon.hackathlon.controller;
 
 import com.hackathlon.hackathlon.dto.requests.registrationDtos.RegistrationRequestDto;
-import com.hackathlon.hackathlon.service.RegistrationService;
+import com.hackathlon.hackathlon.entity.*;
+import com.hackathlon.hackathlon.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.*;
+import java.util.*;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.DateTime;
+
 @RestController
 @RequestMapping("/event/{eventID}/registrations")
 @RequiredArgsConstructor
 public class RegistrationController {
     private final RegistrationService registrationService;
+    private final EventService eventService;
 
     @PostMapping
     private ResponseEntity<?> create(@PathVariable Long eventID, @RequestBody RegistrationRequestDto registrationRequestDto) {
-        try {
-            System.out.println(registrationRequestDto);
-            registrationService.create(eventID, registrationRequestDto);
-//            return ResponseEntity.created(URI.create("/event/" + event.getID())).body("");
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (DataIntegrityViolationException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        Optional<Event> event = eventService.getById(eventID);
+        if (event.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            Date today = Calendar.getInstance().getTime();
+            if (event.get().getRegistrationsNotAfter().after(today)) {
+                return new ResponseEntity<>(HttpStatus.METHOD_NOT_ALLOWED);
+            }
         }
+        Registration registration = registrationService.create(eventID, registrationRequestDto);
+        return ResponseEntity.created(URI.create("/event/" + registration.getEvent().getID() + "/registrations/" + registration.getUUID())).body("");
     }
 }
