336d6a5cc7de1859668a40eecb1eebfd752c7a75
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-02-22 14:17:38 +0100

    Add weekReport dto

---
+++ src/main/java/com/hackathlon/hackathlon/dto/requests/eventDtos/WeekReportRequestDto.java
@@ -0,0 +1,8 @@
+package com.hackathlon.hackathlon.dto.requests.eventDtos;
+
+import com.hackathlon.hackathlon.entity.enums.*;
+
+public class WeekReportRequestDto {
+    private WeekStatus status;
+    private String comment;
+}
