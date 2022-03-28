db82d3c347fa09e3030d493e17f5db09b95fe3f1
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-03-28 09:30:01 +0200

    Add exception handlers

---
+++ src/main/java/com/hackathlon/hackathlon/exception/ErrorDetails.java
@@ -0,0 +1,15 @@
+package com.hackathlon.hackathlon.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+    private String message;
+    private Date timestamp;
+}
---
+++ src/main/java/com/hackathlon/hackathlon/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.hackathlon.hackathlon.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = IllegalStateException.class)
+    public ResponseEntity<ErrorDetails> handleIllegalStateException(IllegalStateException ex) {
+        ErrorDetails errorDetails = this.prepareErrorDetails(ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity<ErrorDetails> handleNoSuchElementException(NoSuchElementException ex) {
+        ErrorDetails errorDetails = this.prepareErrorDetails(ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = NumberFormatException.class)
+    public ResponseEntity<ErrorDetails> handleNumberFormatException(NumberFormatException ex) {
+        ErrorDetails errorDetails = this.prepareErrorDetails(ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    private ErrorDetails prepareErrorDetails(Exception exception){
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setTimestamp(new Date());
+        errorDetails.setMessage(exception.getMessage());
+        return errorDetails;
+    }
+}
